8c251250efa13c80a3a80af48945a8726d05ccd6
EC2 Default User <ec2-user@ip-172-31-29-144.us-east-2.compute.internal>
2021-02-22 04:02:59 +0000

    [HW2] upcase movie before db call

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ class Movie < ActiveRecord::Base
     
     def self.with_ratings(ratings_list, header: nil)
         ratings = ratings_list.present? ? ratings_list.keys : all_ratings
+        ratings = ratings.map { |rating| rating.upcase }
         return self.where(rating: ratings) unless header.present?
         return self.where(rating: ratings).order(header) if header.present?
     end
